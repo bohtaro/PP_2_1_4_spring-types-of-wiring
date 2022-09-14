0aac62704f80749597953577efe2306dd9a3826d
Bohtaro <bohtarovaha@gmail.com>
2022-09-14 10:00:07 +0300

    добавил другие способы связывания

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
-    @Autowired
     private Duck5 duck5;
     @Override
     public String toString() {
         return ", в зайце утка " + duck5.toString();
+    }
+    @Autowired
+    public void setDuck(Duck5 duck5) {
+        this.duck5 = duck5;
     }
 }
